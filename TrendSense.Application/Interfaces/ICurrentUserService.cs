0fb3504bb9d56abe0b5f64db1ee17697c01d2c0b
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-02 23:11:04 +0300

    Создание интерфейса ICurrentUserService

---
+++ TrendSense.Application/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace TrendSense.Application.Interfaces
+{
+    public interface ICurrentUserService
+    {
+        Guid UserId { get; }
+        string UserName { get; }
+        bool IsAuthenticated { get; }
+    }
+}
