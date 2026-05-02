e8ad5514f3b0cfa31044f4526f2214675f9057fe
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-02 23:11:49 +0300

    Создание команды и обработчика добавления WatchList

---
+++ TrendSense.Application/Features/WatchLists/Commands/CreateWatchListCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TrendSense.Domain;
+
+namespace TrendSense.Application.Features.WatchLists.Commands
+{
+    public class CreateWatchListCommand : IRequest<Guid>
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ TrendSense.Application/Features/WatchLists/Commands/CreateWatchListCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using TrendSense.Application.Interfaces;
+using TrendSense.Domain;
+
+namespace TrendSense.Application.Features.WatchLists.Commands
+{
+    public class CreateWatchListCommandHandler : IRequestHandler<CreateWatchListCommand, Guid>
+    {
+        private readonly IAppDbContext _dbContext;
+        private readonly ICurrentUserService _currentUser;
+
+        public CreateWatchListCommandHandler(IAppDbContext dbContext, ICurrentUserService currentUser) =>
+            (_dbContext, _currentUser) = (dbContext, currentUser);
+
+        public async Task<Guid> Handle(CreateWatchListCommand request, CancellationToken cancellationToken)
+        {
+            var watchList = new WatchList
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                UserId = _currentUser.UserId
+            };
+
+            await _dbContext.WatchLists.AddAsync(watchList, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return watchList.Id;
+        }
+    }
+}
