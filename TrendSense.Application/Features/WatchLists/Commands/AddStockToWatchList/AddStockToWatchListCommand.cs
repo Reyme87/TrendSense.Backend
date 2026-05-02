2c1490b9adb5164ab93c8efdc1ea0a6a071f6749
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-03 00:30:53 +0300

    Создание команды и обработчика добавления Stock в WatchList

---
+++ TrendSense.Application/Features/WatchLists/Commands/AddStockToWatchList/AddStockToWatchListCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace TrendSense.Application.Features.WatchLists.Commands.AddStockToWatchList
+{
+    public class AddStockToWatchListCommand : IRequest<Unit>
+    {
+        public Guid WatchListId { get; set; }
+        public Guid StockId { get; set; }
+    }
+}
---
+++ TrendSense.Application/Features/WatchLists/Commands/AddStockToWatchList/AddStockToWatchListCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TrendSense.Application.Common.Exceptions;
+using TrendSense.Application.Interfaces;
+using TrendSense.Domain;
+
+namespace TrendSense.Application.Features.WatchLists.Commands.AddStockToWatchList
+{
+    public class AddStockToWatchListCommandHandler : IRequestHandler<AddStockToWatchListCommand, Unit>
+    {
+        private readonly IAppDbContext _dbContext;
+        private readonly ICurrentUserService _currentUser;
+
+        public AddStockToWatchListCommandHandler(IAppDbContext dbContext, ICurrentUserService currentUser) =>
+            (_dbContext, _currentUser) = (dbContext, currentUser);
+
+        public async Task<Unit> Handle(AddStockToWatchListCommand request, CancellationToken cancellationToken)
+        {
+            var watchList = await _dbContext.WatchLists
+                .Include(x => x.Items)
+                .FirstOrDefaultAsync(x => x.Id == request.WatchListId && x.UserId == _currentUser.UserId, cancellationToken);
+
+            if (watchList == null)
+            {
+                throw new NotFoundException(nameof(WatchList), request.WatchListId);
+            }
+
+            var exists = watchList.Items.Any(x => x.StockId == request.StockId);
+
+            if (exists)
+            {
+                return Unit.Value;
+            }
+
+            watchList.Items.Add(new WatchListItem
+            {
+                Id = Guid.NewGuid(),
+                StockId = request.StockId,
+                WatchListId = request.WatchListId,
+                AddedAt = DateTime.UtcNow
+            });
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
