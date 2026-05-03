8d7a13b488e04d80c8e46b103997d30151bde72c
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-03 15:31:33 +0300

    Создание команды, обработчика и DTO для запроса получения WatchLists

---
+++ TrendSense.Application/Features/WatchLists/Queries/GetWatchLists/GetWatchListsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace TrendSense.Application.Features.WatchLists.Queries.GetWatchLists
+{
+    public class GetWatchListsQuery : IRequest<WatchListVm>
+    {
+    }
+}
---
+++ TrendSense.Application/Features/WatchLists/Queries/GetWatchLists/WatchListLookupDto.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using TrendSense.Application.Common.Mappings;
+using TrendSense.Domain;
+
+namespace TrendSense.Application.Features.WatchLists.Queries.GetWatchLists
+{
+    public class WatchListLookupDto : IMapWith<WatchList>
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<WatchList, WatchListLookupDto>()
+                .ForMember(listDto => listDto.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(listDto => listDto.Name, opt => opt.MapFrom(src => src.Name));
+        }
+    }
+}
