a883a0816aab99dad83a1dc11a2154ddcc330791
Reyme87 <astafev.ivan.0505@mail.ru>
2026-05-06 21:12:50 +0300

    Создание контроллеров для Auth и WatchLists

---
+++ TrendSense.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TrendSense.Application.Features.Auth.Commands.Login;
+using TrendSense.Application.Features.Auth.Commands.Register;
+
+namespace TrendSense.WebApi.Controllers
+{
+    [Route("api/auth")]
+    public class AuthController : BaseController
+    {
+        public AuthController(IMediator mediator) : base(mediator) { }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<Guid>> Register(RegisterUserCommand command)
+        {
+            var userId = await Mediator.Send(command);
+            return Ok(id);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResultDto>> Login(LoginUserCommand command)
+        {
+            var authResult = await Mediator.Send(command);
+            return Ok(authResult);
+        }
+    }
+}
---
+++ TrendSense.WebApi/Controllers/WatchListsController.cs
@@ -0,0 +1,70 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TrendSense.Application.Features.WatchLists.Commands.AddStockToWatchList;
+using TrendSense.Application.Features.WatchLists.Commands.CreateWatchList;
+using TrendSense.Application.Features.WatchLists.Commands.DeleteWatchList;
+using TrendSense.Application.Features.WatchLists.Commands.RemoveStockFromWatchList;
+using TrendSense.Application.Features.WatchLists.Queries.GetWatchLists;
+
+namespace TrendSense.WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/watchlists")]
+    public class WatchListsController : BaseController
+    {
+        public WatchListsController(IMediator mediator) : base(mediator) { }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create(CreateWatchListCommand command)
+        {
+            var id = await Mediator.Send(command);
+            return Ok(id);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<WatchListVm>> Get()
+        {
+            var result = await Mediator.Send(new GetWatchListsQuery());
+            return Ok(result);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            var command = new DeleteWatchListCommand
+            {
+                WatchListId = id
+            };
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpPost("{id:guid}/stocks")]
+        public async Task<ActionResult> AddStock(Guid id, AddStockToWatchListCommand command)
+        {
+            if (id != command.WatchListId)
+            {
+                return BadRequest();
+            }
+
+            await Mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:guid}/stocks/{stockId:guid}")]
+        public async Task<ActionResult> RemoveStock(Guid id, Guid stockId)
+        {
+            var command = new RemoveStockFromWatchListCommand
+            {
+                WatchListId = id,
+                StockId = stockId
+            };
+
+            await Mediator.Send(command);
+
+            return NoContent(); 
+        }
+    }
+}
