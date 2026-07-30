bda2733560508091ff82088164b348ae16564afa
Reyme87 <astafev.ivan.0505@mail.ru>
2026-07-30 14:52:04 +0300

    Создание ConfigureSwaggerOptions

---
+++ TrendSense.WebApi/ConfigureSwaggerOptions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace TrendSense.WebApi
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => _provider = provider;
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in _provider.ApiVersionDescriptions)
+            {
+                var apiVersion = description.ApiVersion.ToString();
+                options.SwaggerDoc(description.GroupName,
+                    new OpenApiInfo
+                    {
+                        Version = apiVersion,
+                        Title = $"TrendSense API{apiVersion}",
+                        Description = "API for monitoring exchange",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Reyme",
+                            Email = string.Empty
+                        },
+                        License = new OpenApiLicense
+                        {
+                            Name = "Product License"
+                        }
+                    });
+            }
+        }
+    }
+}
