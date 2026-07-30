96c92f40e767d132fdbdeba23bfabafc2a24685e
Reyme87 <astafev.ivan.0505@mail.ru>
2026-07-30 14:52:26 +0300

    Регистрация DI контейнеров и сервисов

--- TrendSense.WebApi/Program.cs
+++ TrendSense.WebApi/Program.cs
@@ -1,15 +1,29 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using TrendSense.Application.Common.UserService;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+using TrendSense.Application;
+using TrendSense.Application.Common.Mappings;
 using TrendSense.Application.Interfaces;
 using TrendSense.Domain;
 using TrendSense.Persistence;
+using TrendSense.Persistence.Common.JwtTokens;
+using TrendSense.Persistence.Common.UserService;
+using TrendSense.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHttpContextAccessor();
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(IAppDbContext).Assembly));
+});
 
-builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddControllers();
 
 builder.Services.AddIdentity<AppUser, IdentityRole<Guid>>()
     .AddEntityFrameworkStores<AppDbContext>()
@@ -31,7 +45,46 @@ builder.Services.AddAuthentication("Bearer")
             IssuerSigningKey = new SymmetricSecurityKey(Convert.FromBase64String(builder.Configuration["Jwt:Key"]!))
         };
     });
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+});
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddApiVersioning();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
 
 var app = builder.Build();
+
+var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<AppDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception e)
+    {
+        /////
+    }
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(config =>
+{
+    foreach(var description in provider.ApiVersionDescriptions)
+    {
+        config.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+        config.RoutePrefix = string.Empty;
+    }
+});
 
 app.Run();
