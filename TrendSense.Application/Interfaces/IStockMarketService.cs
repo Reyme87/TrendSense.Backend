36c1909286fbdba93b457cf32cbda766e75549b5
Reyme87 <astafev.ivan.0505@mail.ru>
2026-08-01 15:17:03 +0300

    Создание интерфейса IStockMarketService

---
+++ TrendSense.Application/Interfaces/IStockMarketService.cs
@@ -0,0 +1,11 @@
+﻿using TrendSense.Application.Dtos;
+
+namespace TrendSense.Application.Interfaces
+{
+    public interface IStockMarketService
+    {
+        public Task<IReadOnlyCollection<StockMarketDataDto>> GetStocksAsync(CancellationToken cancellationToken);
+
+        public Task<StockMarketDataDto> GetStockAsync(string ticker, CancellationToken cancellationToken);
+    }
+}
