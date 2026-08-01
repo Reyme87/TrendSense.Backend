83461b2999a483e33a6c8ac053a8e77e4648d329
Reyme87 <astafev.ivan.0505@mail.ru>
2026-08-01 15:17:17 +0300

    Создание Dto StockMarketDataDto

---
+++ TrendSense.Application/Dtos/StockMarketDataDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TrendSense.Application.Dtos
+{
+    public class StockMarketDataDto
+    {
+        public string TickerSymbol { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public decimal LastPrice { get; set; }
+        public decimal PreviousPrice { get; set; }
+
+    }
+}
