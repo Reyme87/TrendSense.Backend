278713fb80fe57282c5481ad4c2ec9bd4c30da1e
Reyme87 <astafev.ivan.0505@mail.ru>
2026-08-02 15:06:52 +0300

    Создание классов MoexBlock и MoexMarketDataResponse

---
+++ TrendSense.Infrastructure/Moex/MoexBlock.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TrendSense.Infrastructure.Moex
+{
+    public class MoexBlock
+    {
+        [JsonPropertyName("columns")]
+        public List<string> Columns { get; set; } = [];
+
+        [JsonPropertyName("data")]
+        public List<List<JsonElement>> Data { get; set; } = [];
+    }
+}
---
+++ TrendSense.Infrastructure/Moex/MoexMarketDataResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TrendSense.Infrastructure.Moex
+{
+    public class MoexMarketDataResponse
+    {
+        [JsonPropertyName("securities")]
+        public MoexBlock? Securities { get; set; }
+
+        [JsonPropertyName("marketdata")]
+        public MoexBlock MarketData { get; set; } = null!;
+    }
+}
