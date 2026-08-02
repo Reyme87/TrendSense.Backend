e04600b080f4f65a4b0b3541c9e676c38adf7f44
Reyme87 <astafev.ivan.0505@mail.ru>
2026-08-02 15:06:23 +0300

    Создание Moex Dtos

---
+++ TrendSense.Infrastructure/Moex/Dtos/MoexSecurityDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TrendSense.Infrastructure.Moex.Dtos
+{
+    public class MoexSecurityDto
+    {
+        public string SecId { get; set; } = null!;
+        public string BoardId { get; set; } = null!;
+        public string ShortName { get; set; } = null!;
+        public string Isin { get; set; } = null!;
+        public string CurrencyId { get; set; } = null!;
+    }
+}
