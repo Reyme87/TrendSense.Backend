fc87344a3deaf4c9a48f499f9a2b7e16330e1ffe
Reyme87 <astafev.ivan.0505@mail.ru>
2026-08-04 16:24:58 +0300

    Изменение интерфейсов

--- TrendSense.Application/Interfaces/IStockMarketService.cs
+++ TrendSense.Application/Interfaces/IStockMarketService.cs
@@ -5,5 +5,7 @@ namespace TrendSense.Application.Interfaces
     public interface IStockMarketService
     {
         public Task<StockMarketInfo?> GetStockAsync(string secId, CancellationToken cancellationToken);
+
+        public Task<IReadOnlyList<StockMarketInfo?>> GetStocksListAsync(CancellationToken cancellationToken);
     }
 }
