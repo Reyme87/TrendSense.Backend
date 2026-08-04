051a49884cdb0f7d6cc4473bcebf3cac573265bf
Reyme87 <astafev.ivan.0505@mail.ru>
2026-08-04 14:01:04 +0300

    Создание запроса и обработчика GetStock

---
+++ TrendSense.Application/Features/Stocks/Queries/GetStockQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TrendSense.Application.Dtos;
+
+namespace TrendSense.Application.Features.Stocks.Queries
+{
+    public class GetStockQuery : IRequest<StockMarketInfo?>
+    {
+        public string SecId { get; set; } = null!;
+    }
+}
