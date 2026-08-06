9e25a95ce6fe31edcc2255a0754881c4e24ddaad
Reyme87 <astafev.ivan.0505@mail.ru>
2026-08-06 14:28:10 +0300

    Создание GetDbStocksQuery

---
+++ TrendSense.Application/Features/Stocks/Queries/GetDbStocks/GetDbStocksQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using System.Data.Entity;
+using TrendSense.Application.Interfaces;
+
+namespace TrendSense.Application.Features.Stocks.Queries.GetDbStocks
+{
+    public class GetDbStocksQueryHandler : IRequestHandler<GetDbStocksQuery, IReadOnlyList<StockDto>>
+    {
+        public IAppDbContext _dbContext;
+
+        public GetDbStocksQueryHandler(IAppDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task<IReadOnlyList<StockDto>> Handle(GetDbStocksQuery request, CancellationToken cancellationToken)
+        {
+            return _dbContext.Stocks
+                .AsNoTracking()
+                .Select(stock => new StockDto
+                {
+                    Id = stock.Id,
+                    TickerSymbol = stock.TickerSymbol,
+                    Name = stock.Name,
+                    Exchange = stock.Exchange,
+                    LastPrice = stock.LastPrice,
+                    DayChange = stock.DayChange,
+                    DayChangePercent = stock.DayChangePercent,
+                    UpdatedAt = stock.UpdatedAt
+                })
+                .ToList();
+        }
+    }
+}
