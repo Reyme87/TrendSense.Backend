f08764ca8e7cf6a34cd91bfd685c1d42d686c95d
Reyme87 <astafev.ivan.0505@mail.ru>
2026-08-13 14:11:29 +0300

    Создание запроса получения истории акций

---
+++ TrendSense.Application/Features/Stocks/Queries/GetPriceHistory/GetPriceHistoryQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System.Data.Entity;
+using TrendSense.Application.Interfaces;
+
+namespace TrendSense.Application.Features.Stocks.Queries.GetPriceHistory
+{
+    public class GetPriceHistoryQueryHandler : IRequestHandler<GetPriceHistoryQuery, IReadOnlyList<PriceHistoryDto>>
+    {
+        private readonly IAppDbContext _dbContext;
+
+        public GetPriceHistoryQueryHandler(IAppDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task<IReadOnlyList<PriceHistoryDto>> Handle(GetPriceHistoryQuery request, CancellationToken cancellationToken)
+        {
+            return _dbContext.History.AsNoTracking()
+                                   .Where(x => x.StockId == request.StockId)
+                                   .OrderBy(x => x.RecordedAt)
+                                   .Select(x => new PriceHistoryDto
+                                   {
+                                       Price = x.Price,
+                                       RecoredAt = x.RecordedAt,
+                                   })
+                                   .ToList();
+        }
+    }
+}
---
+++ TrendSense.Application/Features/Stocks/Queries/GetPriceHistory/PriceHistoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TrendSense.Application.Features.Stocks.Queries.GetPriceHistory
+{
+    public class PriceHistoryDto
+    {
+        public double Price { get; set; }
+        public DateTime RecoredAt { get; set; }
+    }
+}
